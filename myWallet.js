5510cdcaf978a6fc300e96a8e08aff5e17409814
mbarraldevs <mbarraldevs@outlook.com>
2025-10-17 20:29:15 +0200

    add deploy

--- myWallet.js
+++ myWallet.js
@@ -152,29 +152,3 @@ class WalletConnector {
     }
   }
 }
-
-// Example usage:
-/*
-const wallet = new WalletConnector();
-
-// Connect to wallet
-wallet.connect().then(() => {
-  console.log('Connected!');
-  wallet.getBalance();
-}).catch(error => {
-  console.error(error);
-});
-
-// Sign a message
-wallet.signMessage('Hello Web3!').catch(error => {
-  console.error(error);
-});
-
-// Send transaction (requires amount in Wei)
-wallet.sendTransaction('0xRecipientAddress', '1000000000000000000').catch(error => {
-  console.error(error);
-});
-
-// Disconnect
-wallet.disconnect();
-*/
